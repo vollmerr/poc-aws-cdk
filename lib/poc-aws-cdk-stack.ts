1e6180f67cd893d5711d100c0962b880c7c5464d
vollmerr <vollmerr89@gmail.com>
2021-08-17 21:08:49 -0700

    remove oai

--- lib/poc-aws-cdk-stack.ts
+++ lib/poc-aws-cdk-stack.ts
@@ -1,8 +1,12 @@
 import * as cdk from "@aws-cdk/core";
 import * as codebuild from "@aws-cdk/aws-codebuild";
 import * as iam from "@aws-cdk/aws-iam";
+import * as route53 from "@aws-cdk/aws-route53";
+import * as targets from "@aws-cdk/aws-route53-targets";
 import * as s3 from "@aws-cdk/aws-s3";
 import * as cloudfront from "@aws-cdk/aws-cloudfront";
+import * as acm from "@aws-cdk/aws-certificatemanager";
+import * as cloudwatch from "@aws-cdk/aws-cloudwatch";
 import * as codepipeline from "@aws-cdk/aws-codepipeline";
 import * as codepipelineActions from "@aws-cdk/aws-codepipeline-actions";
 import * as pipelines from "@aws-cdk/pipelines";
@@ -27,22 +31,42 @@ const config = {
   cloudfront: {
     distributionId: "E1QGVGRXYXX8JZ",
   },
+  route53: {
+    subdomain: "staging",
+    domain: "vollmerr.com",
+  },
 };
 
+/**
+ * Targets following existing resources:
+ * - github repo (config.github.*)
+ * - s3 bucket (config.s3.bucketName) -> requires public access
+ * - cloudfront distribution (config.cloudfront.distributionId)
+ */
 export class PocAwsCdkStack extends cdk.Stack {
+  // private distributionId: string;
+
   constructor(scope: cdk.Construct, id: string, props: cdk.StackProps) {
     super(scope, id, props);
 
-    this.initializeBucket(id);
+    this.initializeResources(id);
     this.onPullRequest();
     this.onPullRequestMerged();
   }
 
   // initializes a s3 bucket (existing one) with cloudfront access
-  private initializeBucket(id: string) {
-    const cloudfrontOAI = new cloudfront.OriginAccessIdentity(this, "OAI", {
-      comment: `OAI for ${id}`,
-    });
+  private initializeResources(id: string) {
+    // const subdomainName = config.route53.subdomain
+    //   ? `${config.route53.subdomain}.`
+    //   : "";
+    // const domainName = `www.${subdomainName}${config.route53.domain}`;
+    // const zone = route53.HostedZone.fromLookup(this, "Zone", {
+    //   domainName: config.route53.domain,
+    // });
+
+    // const cloudfrontOAI = new cloudfront.OriginAccessIdentity(this, "OAI", {
+    //   comment: `OAI for ${id}`,
+    // });
 
     const bucket = s3.Bucket.fromBucketName(
       this,
@@ -52,15 +76,85 @@ export class PocAwsCdkStack extends cdk.Stack {
 
     const bucketPolicy = new iam.PolicyStatement({
       actions: ["s3:GetBucket*", "s3:GetObject*", "s3:List*"],
-      principals: [
-        new iam.CanonicalUserPrincipal(
-          cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId
-        ),
-      ],
+      // principals: [
+      //   new iam.CanonicalUserPrincipal(
+      //     cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId
+      //   ),
+      // ],
       resources: [bucket.arnForObjects("*")],
     });
 
     bucket.addToResourcePolicy(bucketPolicy);
+
+    // // TLS certificate
+    // const { certificateArn } = new acm.DnsValidatedCertificate(
+    //   this,
+    //   "SiteCertificate",
+    //   {
+    //     domainName,
+    //     hostedZone: zone,
+    //     region: "us-east-1", // Cloudfront only checks this region for certificates.
+    //   }
+    // );
+
+    // // Specifies you want viewers to use HTTPS & TLS v1.1 to request your objects
+    // const viewerCertificate = cloudfront.ViewerCertificate.fromAcmCertificate(
+    //   {
+    //     certificateArn,
+    //     env: {
+    //       account: cdk.Aws.ACCOUNT_ID,
+    //       region: cdk.Aws.REGION,
+    //     },
+    //     metricDaysToExpiry: () =>
+    //       new cloudwatch.Metric({
+    //         metricName: "TLS Viewer Certificate Expired",
+    //         namespace: "TLS Viewer Certificate Validity",
+    //       }),
+    //     node: this.node,
+    //     stack: this,
+    //   },
+    //   {
+    //     aliases: [domainName],
+    //     securityPolicy: cloudfront.SecurityPolicyProtocol.TLS_V1_1_2016,
+    //     sslMethod: cloudfront.SSLMethod.SNI,
+    //   }
+    // );
+
+    // // CloudFront distribution
+    // const distribution = new cloudfront.CloudFrontWebDistribution(
+    //   this,
+    //   "SiteDistribution",
+    //   {
+    //     originConfigs: [
+    //       {
+    //         behaviors: [
+    //           {
+    //             allowedMethods:
+    //               cloudfront.CloudFrontAllowedMethods.GET_HEAD_OPTIONS,
+    //             compress: true,
+    //             isDefaultBehavior: true,
+    //           },
+    //         ],
+    //         s3OriginSource: {
+    //           originAccessIdentity: cloudfrontOAI,
+    //           s3BucketSource: bucket,
+    //         },
+    //       },
+    //     ],
+    //     viewerCertificate,
+    //   }
+    // );
+
+    // // Route53 alias record for the CloudFront distribution
+    // new route53.ARecord(this, "SiteAliasRecord", {
+    //   recordName: domainName,
+    //   target: route53.RecordTarget.fromAlias(
+    //     new targets.CloudFrontTarget(distribution)
+    //   ),
+    //   zone,
+    // });
+
+    // this.distributionId = distribution.distributionId;
   }
 
   // base policy for running codebuild
@@ -130,19 +224,23 @@ export class PocAwsCdkStack extends cdk.Stack {
       ],
     });
 
-    const project = new codebuild.Project(this, "portalSystemPullRequestMerged", {
-      source,
-      environment: {
-        buildImage: codebuild.LinuxBuildImage.STANDARD_5_0,
-        privileged: true,
-        environmentVariables: {
-          CLOUDFRONT_DISTRO_ID: { value: config.cloudfront.distributionId },
+    const project = new codebuild.Project(
+      this,
+      "portalSystemPullRequestMerged",
+      {
+        source,
+        environment: {
+          buildImage: codebuild.LinuxBuildImage.STANDARD_5_0,
+          privileged: true,
+          environmentVariables: {
+            CLOUDFRONT_DISTRO_ID: { value: config.cloudfront.distributionId },
+          },
         },
-      },
-      buildSpec: codebuild.BuildSpec.fromSourceFilename(
-        "./buildspecs/pull-request-merged.yml"
-      ),
-    });
+        buildSpec: codebuild.BuildSpec.fromSourceFilename(
+          "./buildspecs/pull-request-merged.yml"
+        ),
+      }
+    );
 
     const codeBuildPolicy = this.getCodeBuildBasePolicy();
     project.addToRolePolicy(codeBuildPolicy);
